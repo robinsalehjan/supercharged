997caee66c30c1eee030092f9a38e56900ab1c77
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2020-12-10 21:27:48 +0100

    Remov if-statement in update script

--- update.sh
+++ update.sh
@@ -1,11 +1,13 @@
 cp ~/.gitconfig .
 cp ~/.gitignore_global .
 cp ~/.bash_profile .
+
 echo "[brew]: UPDATING OUTDATED PACKAGES"
 brew update && brew upgrade `brew outdated`
+
 echo "[brew cask]: UPDATING OUTDATED CASKS"
 brew upgrade --cask
-if ! [[ $(ps aux | grep gcloud | grep -vc gcloud) > 0 ]]; then exit 0; else $("gcloud components update"); fi
 brew cleanup
 brew bundle --force cleanup
+
 gcloud components update
