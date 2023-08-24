342eb880e9c4b9a217d720bbf7c98bdf05ca40f3
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2023-08-24 14:27:08 +0200

    Remove `softwareupdate` in `update.sh`

--- update.sh
+++ update.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-echo '[brew]: UPDATING OUTDATED PACKAGES'
+echo 'UPDATING BREW PACKAGES'
 brew update && brew upgrade `brew outdated`
 
-echo '[brew cask]: UPDATING OUTDATED CASKS'
+echo 'UPDATING BREW CASKS'
 brew upgrade --cask
 brew cleanup
 
-softwareupdate --all --install
-
+echo 'UPDATING GCLOUD COMPONENTS'
 gcloud components update
