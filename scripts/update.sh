6d00d90e2dc47caf0c8b583e7d64fea6daafa7d9
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2023-11-30 13:43:41 +0100

    Update `sdkman` through `update.sh` script
    Source `sdkman` before updating

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,8 +6,12 @@ brew update && brew upgrade `brew outdated`
 echo 'UPDATING BREW CASKS'
 brew upgrade --cask && brew cleanup
 
-echo 'UPDATING NPM ASDF PLUGINS'
+echo 'UPDATING ASDF PLUGINS'
 asdf plugin update --all
 
 echo 'UPDATING GCLOUD COMPONENTS'
 gcloud components update
+
+echo 'UPDATING SDKMAN'
+source $HOME/.sdkman/bin/sdkman-init.sh
+sdk update
