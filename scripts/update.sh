98336001db641b600040f17f25a320c97ec7f21e
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2025-01-10 10:53:30 +0100

    Run `brew` commands in background processes

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,16 +1,25 @@
 #!/bin/zsh
 
+source "$(dirname "$0")/utils.sh"
+
+setup_logging() {
+    local log_file="$HOME/.supercharged_install.log"
+    exec 1> >(tee -a "$log_file")
+    exec 2> >(tee -a "$log_file" >&2)
+    echo "Installation started at $(date)"
+}
+
 echo 'UPDATING BREW PACKAGES'
-brew update && brew upgrade `brew outdated`
+brew update && brew upgrade `brew outdated` &
 
 echo 'UPDATING BREW CASKS'
-brew upgrade --cask && brew cleanup
+brew upgrade --cask && brew cleanup &
 
 echo 'UPDATING ASDF PLUGINS'
-asdf plugin update --all
+asdf plugin update --all &
 
-echo 'RUNNING ASDF RESHIM WITH SUDO PRIVELEGES'
-sudo asdf reshim
+echo 'RUNNING ASDF RESHIM WITH SUDO PRIVILEGES'
+sudo asdf reshim &
 
 echo 'UPDATING GCLOUD COMPONENTS'
-gcloud components update
+gcloud components update &
